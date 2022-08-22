7714f2415870b031c06e58c46e6953f1aea58790
Surya Prakash <96982121+SuryaPrakash-05@users.noreply.github.com>
2022-08-22 12:31:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function cardDisplay(element){
+    let child = element.childNodes;    
+    let obj = child[1];
+    let bg = window.getComputedStyle(element).backgroundImage;
+    element.store = bg;
+    element.style.backgroundImage="url('')";
+    let color = window.getComputedStyle(obj).backgroundColor;
+    element.style.backgroundColor = color; 
+    child[3].style.display="block";
+}
+function reDo(element){
+    element.style.backgroundImage = element.store;
+    let child = element.childNodes;
+    child[3].style.display="none";
+}
+
+function displayChat(element){
+    let child = element.childNodes;
+    let disp = window.getComputedStyle(child[2]).display;
+    console.log(disp);
+    if(disp=="none"){
+         child[2].style.display= "block";
+    }else{
+        child[2].style.display = "none";
+    }
+}
